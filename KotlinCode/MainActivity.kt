5f1a3b5d5da38169088df3d08300bb9f8ab7a6cb
Akash-Singh-Panwar <57684187+Akash-Singh-Panwar@users.noreply.github.com>
2022-02-17 22:23:06 +0530

    Create MainActivity.kt

---
+++ KotlinCode/MainActivity.kt
@@ -0,0 +1,20 @@
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.widget.Toolbar
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+           val intent = Intent(this@MainActivity, MainActivity2::class.java)
+            startActivity(intent)
+            finish()
+        },5000)
+    }
+}
